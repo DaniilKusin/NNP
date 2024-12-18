784bcd5ed49376f71b8021ae24636c50eb895ee1
evkakiy445 <kloudmane@gmail.com>
2024-12-18 17:14:21 +0300

    about working

--- NNP/NNP/urls.py
+++ NNP/NNP/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', include('image_analysis.urls')),
     path('user_profile/', include('user_profile.urls')),
     path('image-history/', include('image_history.urls')),
+    path('about/', include('about.urls')),
 ]
